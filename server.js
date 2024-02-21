c5285a3647c4345e55c5c6ce0bbeab1a85ba0597
antoineleclerc <plkode.dev@protonmail.com>
2024-02-21 23:29:04 +0100

    functional

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const io = require('socket.io')(3000)
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
 
-const users = {}
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = 3000;
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+const users = {};
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    socket.on("new-user", (name) => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-connected", name);
+    });
+    socket.on("send-chat-message", (message) => {
+        socket.broadcast.emit("chat-message", {
+            message: message,
+            name: users[socket.id],
+        });
+    });
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user-disconnected", users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
